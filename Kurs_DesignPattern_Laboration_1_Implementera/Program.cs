7ae4d0196749f51cba004d94a12971859dc1e86d
mabl0200 <100345848+mabl0200@users.noreply.github.com>
2022-06-21 10:12:38 +0200

    Add project files.

---
+++ Kurs_DesignPattern_Laboration_1_Implementera/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kurs_DesignPattern_Laboration_1_Implementera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
